f7e6e63d6721358d0139e04c6463a1768bb18373
poonammirchandani123 <147245184+poonammirchandani123@users.noreply.github.com>
2024-08-18 15:28:16 +0100

    Add files via upload

--- DatePicker.java
+++ DatePicker.java
@@ -14,11 +14,9 @@ public class DatePicker {
         driver.manage().window().maximize();
         System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver.exe");
 
-
         driver.switchTo().frame(0);
 
-        
-  driver.findElement(By.xpath("//input[@id='datepicker']")).sendKeys("04/05/2024"); //mm/yy//yyyy
+        driver.findElement(By.xpath("//input[@id='datepicker']")).sendKeys("04/05/2024"); //mm/yy//yyyy
     }
     
 }
