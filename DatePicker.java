6d6969e97180a02a24822236fd6490e007233926
poonammirchandani123 <147245184+poonammirchandani123@users.noreply.github.com>
2024-08-18 15:22:26 +0100

    Add files via upload

---
+++ DatePicker.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class DatePicker {
+
+    public static void main(String[] args) {
+        WebDriver driver=new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        
+        driver.get("https://jqueryui.com/datepicker/");
+        driver.manage().window().maximize();
+        System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver.exe");
+
+
+        driver.switchTo().frame(0);
+
+        By By = null;
+        driver.findElement(By.xpath("//input[@id='datepicker']")).sendKeys("04/05/2024"); //mm/yy//yyyy
+    }
+    
+}
